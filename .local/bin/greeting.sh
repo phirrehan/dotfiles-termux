2fb65cac5012507a9157912fdb13cad0bd1367ec
phirrehan <phirrehan@gmail.com>
2026-08-08 23:30:08 +0500

    changed ansi escape sequence in greeting.sh

--- .local/bin/greeting.sh
+++ .local/bin/greeting.sh
@@ -8,7 +8,7 @@ if [ -s "$file" ]; then
   total_lines=$(awk 'END { print NR }' "$file")
   greeting=$(awk -v n=$((RANDOM % total_lines + 1)) 'NR == n {print; exit}' "$file") # stores text in $file at line $random_line
 fi
-printf '\033[37;5;16m' # Set colour to primary
+printf '\033[87;5m' # Set colour to primary
 if command -v figlet >/dev/null; then
   figlet -tf small "$greeting" | sed 's/^/  /'
 else
