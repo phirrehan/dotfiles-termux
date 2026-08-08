e32645cc4637ee3c4aad43670f636e99ec7f8268
phirrehan <phirrehan@gmail.com>
2026-08-08 16:15:44 +0500

    cleaned up repo and home directory

--- .local/bin/get-timetable.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-TIMETABLE_DIR="$HOME/files/storage/downloads/kdeconnect"
-ls "$TIMETABLE_DIR/"*"-classes.pdf"
--- .local/bin/searXNG.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd $HOME/apps/searxng-docker/
-[ -z "$1" ] && docker compose down
-[ "$1" = "start" ] && docker compose up -d &&
-  sleep 5 && docker compose down && docker compose up -d
