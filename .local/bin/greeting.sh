c72a3b6ddbae0a8d854dd7999faa487d32afdf6b
phirrehan <phirrehan@gmail.com>
2026-08-08 13:19:13 +0500

    added .greeting.txt and fixed greeting.sh

--- .local/bin/greeting.sh
+++ .local/bin/greeting.sh
@@ -4,15 +4,15 @@
 file="$HOME/.local/bin/.greeting.txt"
 greeting="Welcome"
 
-if [ -s "$file" ]; then 
+if [ -s "$file" ]; then
   total_lines=$(awk 'END { print NR }' "$file")
   greeting=$(awk -v n=$((RANDOM % total_lines + 1)) 'NR == n {print; exit}' "$file") # stores text in $file at line $random_line
 fi
-printf '\e[38;5;16m'  # Set colour to primary
-if command -v figlet &>/dev/null; then 
-  figlet -tf small "$greeting" | sed 's/^/  /' 
+printf '\033[37;5;16m' # Set colour to primary
+if command -v figlet >/dev/null; then
+  figlet -tf small "$greeting" | sed 's/^/  /'
 else
   echo "Please install figlet for appropriate message."
 fi
-printf '\e[0m'
-command -v fastfetch &> /dev/null && fastfetch --key-padding-left 5
+printf '\033[0m'
+command -v fastfetch >/dev/null && fastfetch --key-padding-left 5
