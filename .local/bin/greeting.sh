d37914e66bd75fb8b14c66ad2768331125761f8b
phirrehan <phirrehan@gmail.com>
2026-08-18 21:08:52 +0500

    changed color in greeting.sh

--- .local/bin/greeting.sh
+++ .local/bin/greeting.sh
@@ -3,16 +3,17 @@
 # variables
 file="$HOME/.local/bin/.greeting.txt"
 greeting="Welcome"
-
-if [ -s "$file" ]; then
+if [ -s "$file" ]; then 
   total_lines=$(awk 'END { print NR }' "$file")
   greeting=$(awk -v n=$((RANDOM % total_lines + 1)) 'NR == n {print; exit}' "$file") # stores text in $file at line $random_line
 fi
-printf '\033[87;5m' # Set colour to primary
-if command -v figlet >/dev/null; then
-  figlet -tf small "$greeting" | sed 's/^/  /'
+
+# start
+printf '\033[36m'  # Set colour to cyan
+if command -v figlet &>/dev/null; then 
+  figlet -tf small "$greeting" | sed 's/^/  /' 
 else
   echo "Please install figlet for appropriate message."
 fi
 printf '\033[0m'
-command -v fastfetch >/dev/null && fastfetch --key-padding-left 5
+command -v fastfetch &> /dev/null && fastfetch --key-padding-left 5
