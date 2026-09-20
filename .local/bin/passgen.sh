38b12a11b10c870d0db24a2290884283d307d25c
phirrehan <phirrehan@gmail.com>
2026-09-21 01:09:01 +0500

    updated passgen.sh for passime

--- .local/bin/passgen.sh
+++ .local/bin/passgen.sh
@@ -1,17 +1,50 @@
 #!/data/data/com.termux/files/usr/bin/sh
 
 export PASSWORD_STORE_DIR="$HOME/files/Passwords/store"
-passName=$1   # First Argument is name of password
-passLength=$2 # Second Argument is length of password
+
+auth_token="$1"
+passName="$2"
+passLength="$3"
+
+# Require authentication token.
+[ -z "$auth_token" ] && exit 1
+
+# Require password name.
+[ -z "$passName" ] && {
+  echo "Error: password name is required" >&2
+  exit 2
+}
+
+# Default length.
 [ -z "$passLength" ] && passLength=20
 
-# Check Errors
-[[ ! "$passLength" =~ ^[0-9]+$ ]] &&
-  echo "Error: invalid input. expected a number" &&
-  read -p "Press enter to exit." && exit 1
-[ $passLength -le 0 ] &&
-  echo "Error: invalid input. expected a number greater than 0" &&
-  read -p "Press enter to exit." && exit 2
+# Check length.
+case "$passLength" in
+*[!0-9]*)
+  echo "Error: invalid input. expected a number" >&2
+  exit 3
+  ;;
+esac
 
-# Generate password
-pass generate -c "$passName" "$passLength"
+[ "$passLength" -le 0 ] && {
+  echo "Error: invalid input. expected a number greater than 0" >&2
+  exit 4
+}
+
+# Generate password.
+password=$(
+  pass generate -f "$passName" "$passLength" |
+    awk 'NR==2 { gsub(/\x1B\[[0-9;]*[[:alpha:]]/, ""); printf "%s", $0 }'
+)
+
+[ -z "$password" ] && exit 5
+
+echo "Sending generated password to PassIme..." >&2
+
+am broadcast \
+  -n com.example.passime/.PasswordReceiver \
+  -a com.example.passime.PASSWORD \
+  --es token "$auth_token" \
+  --es password "$password"
+
+exit $?
